876ff01c599af836bc4204aecf02ea58e60c3c75
theimpulson <aayushgupta219@gmail.com>
2019-12-31 13:19:35 +0000

    sdm660-common: Import and build vendor hidl|libs of ANT+ and WFD
    Change-Id: I9b49e6e3d8bb276096f2b0a3427fe3fd0da000e3
    Signed-off-by: theimpulson <aayushgupta219@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -99,7 +99,11 @@ PRODUCT_PACKAGES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService
+    AntHalService \
+    com.dsi.ant.antradio_library
+
+PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Audio
 PRODUCT_PACKAGES += \
