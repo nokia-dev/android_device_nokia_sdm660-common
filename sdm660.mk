30af1a09eee860c90c9fb7af60da4a2ee1a2df29
theimpulson <aayushgupta219@gmail.com>
2020-03-17 13:57:47 +0000

    sdm660-common: Import more required libs
    Signed-off-by: theimpulson <aayushgupta219@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -329,11 +329,6 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(COMMON_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
-# Sensors
-PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl:64 \
-    android.hardware.sensors@1.0-service
-
 # Shims
 PRODUCT_PACKAGES += \
     camera.sdm660_shim
